a07de8d03858c6b83cdc11c36b75f5b7eeed85c7
FrancesscaWY <3449997118@qq.com>
2024-02-21 18:01:35 +0800

    成功读取文件并转化为数组

--- try/study.php
+++ try/study.php
@@ -8,38 +8,50 @@
 </head>
 <body>
 <?php
-$dir='D:\workfile\FileRecv\homeworksubmit';
-$submitted=array();
-function getfilecounts($ff){
-    $dir = 'D:\workfile\FileRecv\homeworksubmit'.$ff;
-    $handle = opendir($dir);
-    $i = 0;
-    while(false !== $file=(readdir($handle))){
-        if($file !== '.' && $file != '..')
-        {
-            $i++;
-        }
-    }
-    closedir($handle);
-    return $i;
-}
-   echo getfilecounts( );
-    if(is_dir($dir)){
-    $info=opendir($dir);
-    while(($file=readdir($info))!==false){
-        /*if($file == '.')
-            continue;
-        if($file == '..')
-            continue;*/  //将前面三个点去掉
+// 读取文本1
+//    $file_path="totalsudents.txt";
+//    if(file_exists($file_path)){
+//        $fp=fopen($file_path,"r");
+//        $str=fread($fp,filesize($file_path));
+//        echo $str=str_replace("\r\n","<br />",$str);
+//    }
+//else{
+//    echo "fail";
+//}
 
-       // for($i=1;$i<strlen)
-      //  $submitted = $file."<br>";
-      echo $file."<br>";
-    }
-    closedir($info);
+//读取文本
+$file='D:\PHP\file\try\text\totalsudents.txt';
+$content=file_get_contents($file);
+echo $content."<br>";
+
+//将所有同学名字转化为数组
+$pattern = '/[\x{4e00}-\x{9fa5}]+/u'; // 匹配中文字符的正则表达式
+preg_match_all($pattern, $content, $total);
+$chineseArray = $total[0];
+print_r($chineseArray);
+$total_number=count($total);
+echo $total_number;
+
+//读取文件夹
+$path='D:\workfile\FileRecv\homeworksubmit';
+$submitted=scandir($path);
+    //print_r($submitted);
+$counts=count($submitted)-2;
+echo $counts;
+//将文件夹转化为数组
+$newArray = array_slice($submitted, 2);
+print_r($newArray);
+//纯中文数组
+$pattern = '/[^\x{4e00}-\x{9fa5}]/u'; // 匹配非中文字符的正则表达式
+foreach ($newArray as &$actual) {
+    $actual = preg_replace($pattern, '', $actual);
 }
-    //echo $submitted;
+print_r($newArray);
 ?>
+<script>
+    var javascriptVariable = "<?php echo $counts; ?>"
+    //document.getElementsByTagName('body')[0].innerHTML = javascriptVariable
+</script>
 <p>hello</p>
 </body>
 
