ada79ebd2c96b331bf3d563dba70d3758fb1c369
FrancesscaWY <3449997118@qq.com>
2024-02-22 13:42:07 +0800

    封装后读取转化

--- try/study.php
+++ try/study.php
@@ -1,58 +1,34 @@
-<html>
-<head>
-    <style type="text/css">
-        p{
-            color:rebeccapurple;
-        }
-    </style>
-</head>
-<body>
+
 <?php
-// 读取文本1
-//    $file_path="totalsudents.txt";
-//    if(file_exists($file_path)){
-//        $fp=fopen($file_path,"r");
-//        $str=fread($fp,filesize($file_path));
-//        echo $str=str_replace("\r\n","<br />",$str);
-//    }
-//else{
-//    echo "fail";
-//}
+function get_str_array_list($obj){ //将文本转化为list
+    $pattern='/[\x{4e00}-\x{9fa5}]+/u';
+    preg_match_all($pattern,$obj,$array);
+    $chineseArray=$array[0];
+    $list=implode(', ',$chineseArray);
+    echo $list;
+}
+
+function get_file_array_list($obj_)
+{
+    $initial=scandir($obj_);//读取文件夹下的文件及目录并存储在数组中
+    $number=count($initial)-2;
+    echo $number;
+    $move=array_slice($initial,2);
+    $pattern_='/[^\x{4e00}-\x{9fa5}]/u';
+    foreach ($move as &$end){
+        $end=preg_replace($pattern_,'',$end);
+    }
+    $list_end=implode(', ',$move);
+    print_r($list_end);
+}
 
 //读取文本
 $file='D:\PHP\file\try\text\totalsudents.txt';
 $content=file_get_contents($file);
-echo $content."<br>";
-
-//将所有同学名字转化为数组
-$pattern = '/[\x{4e00}-\x{9fa5}]+/u'; // 匹配中文字符的正则表达式
-preg_match_all($pattern, $content, $total);
-$chineseArray = $total[0];
-print_r($chineseArray);
-$total_number=count($total);
-echo $total_number;
-
+//调用文本转化为list（字符串）函数
+get_str_array_list($content);
 //读取文件夹
 $path='D:\workfile\FileRecv\homeworksubmit';
-$submitted=scandir($path);
-    //print_r($submitted);
-$counts=count($submitted)-2;
-echo $counts;
-//将文件夹转化为数组
-$newArray = array_slice($submitted, 2);
-print_r($newArray);
-//纯中文数组
-$pattern = '/[^\x{4e00}-\x{9fa5}]/u'; // 匹配非中文字符的正则表达式
-foreach ($newArray as &$actual) {
-    $actual = preg_replace($pattern, '', $actual);
-}
-print_r($newArray);
+//调用文件夹转化为list（字符串）函数
+get_file_array_list($path);
 ?>
-<script>
-    var javascriptVariable = "<?php echo $counts; ?>"
-    //document.getElementsByTagName('body')[0].innerHTML = javascriptVariable
-</script>
-<p>hello</p>
-</body>
-
-</html>
