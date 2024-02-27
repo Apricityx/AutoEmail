9e61c6e305d7702556b35d4b74ceb823d860682d
FrancesscaWY <3449997118@qq.com>
2024-02-27 23:13:20 +0800

    弹幕式滚动....

--- forward part/main.php
+++ forward part/main.php
@@ -1,71 +1,92 @@
 <html>
 <head>
     <title>软工3、4班邮件服务平台</title>
+    <meta charset="UTF-8">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <!--    <script src="mainpage.js"></script>-->
     <link rel="stylesheet" href="mainpage.css">
-    <link rel="stylesheet" href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<!--    <link rel="stylesheet" href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css">-->
+    <style>
+        @font-face {
+            font-family: 'font_style01';
+            src: url('font/font_style01.otf');
+        }
+        @font-face {
+            font-family: 'font_style02';
+            src: url('font/font_style02.otf');
+        }
+       body{
+            font-family: 'font_style01';
+        }
+    </style>
 </head>
 <body>
 <div id="title"><h1>软工3、4班邮件服务平台</h1></div>
 <div id="left">1</div>
-<div id="std_info">2<br>3<br>4
-<!--<div class="std" id="div0"></div>-->
-<!--<div class="std" id="div1"></div>-->
-<!--<div class="std" id="div2"></div>-->
-<!--<div class="std" id="div3"></div>-->
-<!--<div class="std" id="div4"></div>-->
-<!--<div class="std" id="div5"></div>-->
-<!--<div class="std" id="div6"></div>-->
-<!--<div class="std" id="div7"></div>-->
-<!--<div class="std" id="div8"></div>-->
-<!--<div class="std" id="div9"></div>-->
-<!--<div class="std" id="div10"></div>-->
-<!--<div class="std" id="div11"></div>-->
-<!--<div class="std" id="div12"></div>-->
-<!--<div class="std" id="div13"></div>-->
-<!--<div class="std" id="div14"></div>-->
-<!--<div class="std" id="div15"></div>-->
-<!--<div class="std" id="div16"></div>-->
-<!--<div class="std" id="div17"></div>-->
-<!--<div class="std" id="div18"></div>-->
-<!--<div class="std" id="div19"></div>-->
-<!--<div class="std" id="div20"></div>-->
-<!--<div class="std" id="div21"></div>-->
-<!--<div class="std" id="div22"></div>-->
-<!--<div class="std" id="div23"></div>-->
-<!--<div class="std" id="div24"></div>-->
-<!--<div class="std" id="div25"></div>-->
-<!--<div class="std" id="div26"></div>-->
-<!--<div class="std" id="div27"></div>-->
-<!--<div class="std" id="div28"></div>-->
-<!--<div class="std" id="div29"></div>-->
-<!--<div class="std" id="div30"></div>-->
-<!--<div class="std" id="div31"></div>-->
-<!--<div class="std" id="div32"></div>-->
-<!--<div class="std" id="div33"></div>-->
-<!--<div class="std" id="div34"></div>-->
-<!--<div class="std" id="div35"></div>-->
-<!--<div class="std" id="div36"></div>-->
-<!--<div class="std" id="div37"></div>-->
-<!--<div class="std" id="div38"></div>-->
-<!--<div class="std" id="div39"></div>-->
-<!--<div class="std" id="div40"></div>-->
-<!--<div class="std" id="div41"></div>-->
-<!--<div class="std" id="div42"></div>-->
-<!--<div class="std" id="div43"></div>-->
-<!--<div class="std" id="div44"></div>-->
-<!--<div class="std" id="div45"></div>-->
-<!--<div class="std" id="div46"></div>-->
-<!--<div class="std" id="div47"></div>-->
-<!--<div class="std" id="div48"></div>-->
-<!--<div class="std" id="div49"></div>-->
-<!--<div class="std" id="div50"></div>-->
-<!--<div class="std" id="div51"></div>-->
-<!--<div class="std" id="div52"></div>-->
-<!--<div class="std" id="div53"></div>-->
-<!--<div class="std" id="div54"></div>-->
-<!--<div class="std" id="div55"></div>-->
-<!--<div class="std" id="div56"></div>-->
+<div id="std_info">
+    <marquee behavior="scroll" direction="left">
+<div class="std" id="div0"><h2>061</h2></div>
+<div class="std" id="div1"><h2>062</h2></div>
+<div class="std" id="div2"><h2>064</h2></div>
+<div class="std" id="div3"><h2>065</h2></div>
+<div class="std" id="div4"><h2>066</h2></div>
+<div class="std" id="div5"><h2>067</h2></div>
+<div class="std" id="div6"><h2>068</h2></div>
+<div class="std" id="div7"><h2>069</h2></div>
+<div class="std" id="div8"><h2>070</h2></div>
+<div class="std" id="div9"><h2>071</h2></div>
+<div class="std" id="div10"><h2>073</h2></div>
+<div class="std" id="div11"><h2>075</h2></div>
+<div class="std" id="div12"><h2>076</h2></div>
+<div class="std" id="div13"><h2>077</h2></div>
+<div class="std" id="div14"><h2>078</h2></div>
+<div class="std" id="div15"><h2>079</h2></div>
+<div class="std" id="div16"><h2>080</h2></div>
+<div class="std" id="div17"><h2>081</h2></div>
+<div class="std" id="div18"><h2>082</h2></div>
+        </br>
+<div class="std" id="div19"><h2>083</h2></div>
+<div class="std" id="div20"><h2>084</h2></div>
+<div class="std" id="div21"><h2>085</h2></div>
+<div class="std" id="div22"><h2>086</h2></div>
+<div class="std" id="div23"><h2>087</h2></div>
+<div class="std" id="div24"><h2>088</h2></div>
+<div class="std" id="div25"><h2>089</h2></div>
+<div class="std" id="div26"><h2>090</h2></div>
+<div class="std" id="div27"><h2>091</h2></div>
+<div class="std" id="div28"><h2>092</h2></div>
+<div class="std" id="div29"><h2>093</h2></div>
+<div class="std" id="div30"><h2>094</h2></div>
+<div class="std" id="div31"><h2>095</h2></div>
+<div class="std" id="div32"><h2>096</h2></div>
+<div class="std" id="div33"><h2>097</h2></div>
+<div class="std" id="div34"><h2>098</h2></div>
+<div class="std" id="div35"><h2>099</h2></div>
+<div class="std" id="div36"><h2>100</h2></div>
+<div class="std" id="div37"><h2>101</h2></div>
+        </br>
+<div class="std" id="div38"><h2>102</h2></div>
+<div class="std" id="div39"><h2>103</h2></div>
+<div class="std" id="div40"><h2>104</h2></div>
+<div class="std" id="div41"><h2>105</h2></div>
+<div class="std" id="div42"><h2>106</h2></div>
+<div class="std" id="div43"><h2>107</h2></div>
+<div class="std" id="div44"><h2>108</h2></div>
+<div class="std" id="div45"><h2>109</h2></div>
+<div class="std" id="div46"><h2>110</h2></div>
+<div class="std" id="div47"><h2>111</h2></div>
+<div class="std" id="div48"><h2>112</h2></div>
+<div class="std" id="div49"><h2>113</h2></div>
+<div class="std" id="div50"><h2>114</h2></div>
+<div class="std" id="div51"><h2>115</h2></div>
+<div class="std" id="div52"><h2>116</h2></div>
+<div class="std" id="div53"><h2>117</h2></div>
+<div class="std" id="div54"><h2>118</h2></div>
+<div class="std" id="div55"><h2>119</h2></div>
+<div class="std" id="div56"><h2>120</h2></div>
+    </marquee>
 </div>
 <!--<div id="footer"></div>-->
 <?php
@@ -93,7 +114,7 @@ function get_file_array_list($obj_)
 }
 
 //读取文本
-$file='D:\PHP\file\forward part\text\totalsudents.txt';
+$file='D:\PHP\file\AutoEmail\forward part\text\totalsudents.txt';
 $content=file_get_contents($file);
 //调用文本转化为list（字符串）函数
 $total_s=get_str_array_list($content);
