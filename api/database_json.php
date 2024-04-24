9b1484e23df1af1ac0123e793b4e8120b1079ce3
FrancesscaWY <3449997118@qq.com>
2024-04-24 09:29:31 +0800

    Feat. 读取所有表并转为JSON.

---
+++ api/database_json.php
@@ -0,0 +1,37 @@
+<?php
+//allow chinese characters
+header("Content-type: text/html; charset=utf-8");
+$file='.\password_.txt';
+$pass=file_get_contents($file);
+$servername = "localhost";
+$username="root";
+$password=$pass;
+$dbname="autoemail";
+$conn = new mysqli($servername,$username,$password,$dbname);
+if($conn->connect_error){
+    die("连接失败：".$conn->connect_error);
+}
+//读取所有的表，将数据库中所有的表以及表中的内容转换为json格式
+$sql = "show tables";
+$result = $conn->query($sql);
+$tables = array();//存放表名和表中的内容
+if($result->num_rows>0){
+    while($row=$result->fetch_assoc()){
+        $table=$row['Tables_in_autoemail'];//表名
+        if($table=='students'){
+            continue;//跳过student表
+        }
+        $sql="select * from $table";
+        $result2=$conn->query($sql);
+        $rows=array();
+        if($result2->num_rows>0){
+            while($row2=$result2->fetch_assoc()){
+                $rows[]=$row2;
+            }
+        }
+            $tables[$table]=$rows;
+    }
+
+}
+echo json_encode($tables);
+$conn->close();
